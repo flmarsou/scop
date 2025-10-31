a0de5c8377a6e1bdd6b6aa9cd480f1a23506e0b5
flmarsou <marsouinflorent12@gmail.com>
2025-10-31 15:49:16 +0100

    Added window initialization/loop/cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include "types.hpp"
 #include "debug.hpp"
 
+#include "Window.hpp"
+
 i32	main()
 {
-	printLog(LogType::Message, "Hello World!");
-	return (0);
+	bool	ret = 0;
+	Window	window;
+
+	try
+	{
+		window.Init(1280, 720, "scop by flmarsou");
+	}
+	catch(const std::exception &error)
+	{
+		printLog(LogType::Error, error.what());
+		ret = 1;
+		goto terminate_scop;
+	}
+
+	while (!window.ShouldClose())
+	{
+		window.Clear();
+		window.SwapBuffers();
+		window.PollEvents();
+	}
+
+	terminate_scop:
+		window.Cleanup();
+
+	return (ret);
 }
