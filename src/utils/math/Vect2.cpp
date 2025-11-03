84b5dbd32ed5cdfea172ed2ff7fbed2de82d4e9d
flmarsou <marsouinflorent12@gmail.com>
2025-11-03 16:16:46 +0100

    Added vector2D

---
+++ src/utils/math/Vect2.cpp
@@ -0,0 +1,162 @@
+#include "Vect2.hpp"
+
+#include <cmath>
+
+// ========================================================================== //
+//                                  Additions                                 //
+// ========================================================================== //
+
+Vect2	Vect2::operator+(const Vect2 &other) const
+{
+	Vect2	tmp(*this);
+
+	tmp.x += other.x;
+	tmp.y += other.y;
+
+	return (tmp);
+}
+
+Vect2	&Vect2::operator+=(const Vect2 &other)
+{
+	x += other.x;
+	y += other.y;
+
+	return (*this);
+}
+
+// ========================================================================== //
+//                                Subtractions                                //
+// ========================================================================== //
+
+Vect2	Vect2::operator-(const Vect2 &other) const
+{
+	Vect2	tmp(*this);
+
+	tmp.x -= other.x;
+	tmp.y -= other.y;
+
+	return (tmp);
+}
+
+Vect2	&Vect2::operator-=(const Vect2 &other)
+{
+	x -= other.x;
+	y -= other.y;
+
+	return (*this);
+}
+
+// ========================================================================== //
+//                               Multiplications                              //
+// ========================================================================== //
+
+Vect2	Vect2::operator*(const Vect2 &other) const
+{
+	Vect2	tmp(*this);
+
+	tmp.x *= other.x;
+	tmp.y *= other.y;
+
+	return (tmp);
+}
+
+Vect2	Vect2::operator*(const f32 scalar) const
+{
+	Vect2	tmp(*this);
+
+	tmp.x *= scalar;
+	tmp.y *= scalar;
+
+	return (tmp);
+}
+
+Vect2	&Vect2::operator*=(const Vect2 &other)
+{
+	x *= other.x;
+	y *= other.y;
+
+	return (*this);
+}
+
+Vect2	&Vect2::operator*=(const f32 scalar)
+{
+	x *= scalar;
+	y *= scalar;
+
+	return (*this);
+}
+
+// ========================================================================== //
+//                                  Divisons                                  //
+// ========================================================================== //
+
+Vect2	Vect2::operator/(const Vect2 &other) const
+{
+	Vect2	tmp(*this);
+
+	tmp.x /= other.x;
+	tmp.y /= other.y;
+
+	return (tmp);
+}
+
+Vect2	Vect2::operator/(const f32 scalar) const
+{
+	Vect2	tmp(*this);
+
+	tmp.x /= scalar;
+	tmp.y /= scalar;
+
+	return (tmp);
+}
+
+Vect2	&Vect2::operator/=(const Vect2 &other)
+{
+	x /= other.x;
+	y /= other.y;
+
+	return (*this);
+}
+
+Vect2	&Vect2::operator/=(const f32 scalar)
+{
+	x /= scalar;
+	y /= scalar;
+
+	return (*this);
+}
+
+// ========================================================================== //
+//                                 Comparisons                                //
+// ========================================================================== //
+
+bool	Vect2::operator==(const Vect2 &other) const
+{
+	return (x == other.x && y == other.y);
+}
+
+bool	Vect2::operator!=(const Vect2 &other) const
+{
+	return (x != other.x || y != other.y);
+}
+
+// ========================================================================== //
+//                                   Methods                                  //
+// ========================================================================== //
+
+f32		Vect2::dot(const Vect2 &other) const
+{
+	return (x * other.x + y * other.y);
+}
+
+f32		Vect2::length() const
+{
+	return (std::sqrt(x * x + y * y));
+}
+
+Vect2	Vect2::normalized() const
+{
+	const f32	len = length();
+
+	return (len != 0 ? *this / len : *this);
+}
---
+++ src/utils/math/Vect2.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+# include "types.hpp"
+
+struct	Vect2
+{
+	Vect2() : x(0.0f), y(0.0f) {};
+	Vect2(const f32 x, const f32 y) : x(x), y(y) {};
+
+	// Additions
+	Vect2	operator+(const Vect2 &other) const;
+	Vect2	&operator+=(const Vect2 &other);
+
+	// Subtractions
+	Vect2	operator-(const Vect2 &other) const;
+	Vect2	&operator-=(const Vect2 &other);
+
+	// Multiplications
+	Vect2	operator*(const Vect2 &other) const;
+	Vect2	operator*(const f32 scalar) const;
+	Vect2	&operator*=(const Vect2 &other);
+	Vect2	&operator*=(const f32 scalar);
+
+	// Divisons
+	Vect2	operator/(const Vect2 &other) const;
+	Vect2	operator/(const f32 scalar) const;
+	Vect2	&operator/=(const Vect2 &other);
+	Vect2	&operator/=(const f32 scalar);
+
+	// Comparisons
+	bool	operator==(const Vect2 &other) const;
+	bool	operator!=(const Vect2 &other) const;
+
+	// Methods
+	/**
+	 * Returns the dot product - a measure of how much two vectors point in the same direction.
+	 * 
+	 * Formula: (x₁ * x₂) + (y₁ * y₂)
+	 * 
+	 * `> 0` -> vectors point in the same direction
+	 * `< 0` -> vectors point in the opposite direction
+	 * `= 0` -> vectors are perpendicular
+	 */
+	f32		dot(const Vect2 &other) const;
+
+	/**
+	 * Returns the magnitude (length) of the vector from the origin.
+	 * 
+	 * Formula: √(x² + y²)
+	 */
+	f32		length() const;
+
+	/**
+	 * Returns a normalized (unit-length) copy of the vector with the same direction but a length of 1.
+	 */
+	Vect2	normalized() const;
+
+	f32	x;
+	f32	y;
+};
