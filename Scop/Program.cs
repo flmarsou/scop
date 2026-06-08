dc6a82cf79a7fbb0399da4f79195ff5ca243d860
flmarsou <marsouinflorent12@gmail.com>
2026-06-08 14:12:07 +0200

    Add default window creation

--- Scop/Program.cs
+++ Scop/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace Scop;
+
+public partial class Program
+{
+    private static IWindow? _window;
+
+    private static void Main()
+    {
+        var options = WindowOptions.Default with
+        {
+            Size  = new Vector2D<int>(800, 600),
+            Title = "Scop - by flmarsou",
+        };
+
+        _window = Window.Create(options);
+
+        _window.Load   += OnLoad;
+        _window.Update += OnUpdate;
+        _window.Render += OnRender;
+
+        _window.Run();
+    }
+}
