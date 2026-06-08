e3a067b125e48d233f033fd54b62c2d6d7f926cb
flmarsou <marsouinflorent12@gmail.com>
2026-06-08 22:29:54 +0200

    Add .obj data structure

---
+++ Scop/Object/ObjData.cs
@@ -0,0 +1,48 @@
+namespace Scop.Object;
+
+public struct ObjVertex(float x, float y, float z)
+{
+    public float X = x;
+    public float Y = y;
+    public float Z = z;
+}
+
+public struct ObjTexCoord(float u, float v)
+{
+    public float U = u;
+    public float V = v;
+}
+
+public struct ObjNormal(float x, float y, float z)
+{
+    public float X = x;
+    public float Y = y;
+    public float Z = z;
+}
+
+public struct ObjFaceVertex(int vertexIndex, int? textureIndex, int? normalIndex)
+{
+    public int  VertexIndex  = vertexIndex;
+    public int? TextureIndex = textureIndex;
+    public int? NormalIndex  = normalIndex;
+}
+
+public struct ObjFace(ObjFaceVertex[] vertices, string? material)
+{
+    public ObjFaceVertex[] Vertices = vertices;
+    public string?         Material = material;
+}
+
+public struct ObjGroup(string name)
+{
+    public string        Name = name;
+    public List<ObjFace> Faces = [];
+}
+
+public struct ObjModel()
+{
+    public List<ObjVertex>   Vertices  = [];
+    public List<ObjTexCoord> TexCoords = [];
+    public List<ObjNormal>   Normals   = [];
+    public List<ObjGroup>    Groups    = [];
+}
