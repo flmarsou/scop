b78f1c35a977a3a8d7867facd3394acb9459fd7a
flmarsou <marsouinflorent12@gmail.com>
2026-06-08 22:35:22 +0200

    Add escape key to exit the program

--- Scop/Core/OnUpdate.cs
+++ Scop/Core/OnUpdate.cs
@@ -1,9 +1,13 @@
+using Scop.Input;
+using Silk.NET.Input;
+
 namespace Scop;
 
 public partial class Program
 {
     private static void OnUpdate(double deltaTime)
     {
-
+        if (InputManager.IsKeyDown(Key.Escape))
+            _window!.Close();
     }
 }
