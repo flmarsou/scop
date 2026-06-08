0b89c70531e7e3af3fac888662d1c2b3a3a4578d
flmarsou <marsouinflorent12@gmail.com>
2026-06-08 15:18:17 +0200

    Init InputManager

--- Scop/Core/OnLoad.cs
+++ Scop/Core/OnLoad.cs
@@ -1,9 +1,12 @@
+using Scop.Input;
+using Silk.NET.Input;
+
 namespace Scop;
 
 public partial class Program
 {
     private static void OnLoad()
     {
-
+        InputManager.Init(_window!.CreateInput());
     }
 }
