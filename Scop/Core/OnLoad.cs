f73a0003c2edf922c69806e62c3f5de4f5d9caf9
flmarsou <marsouinflorent12@gmail.com>
2026-06-08 18:44:29 +0200

    Init ObjectManager

--- Scop/Core/OnLoad.cs
+++ Scop/Core/OnLoad.cs
@@ -1,4 +1,6 @@
 using Scop.Input;
+using Scop.Object;
+using Scop.Utils;
 using Silk.NET.Input;
 
 namespace Scop;
@@ -8,5 +10,16 @@ public partial class Program
     private static void OnLoad()
     {
         InputManager.Init(_window!.CreateInput());
+
+        try
+        {
+            ObjectManager.Init();
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex.ToString());
+
+            _window!.Close();
+        }
     }
 }
