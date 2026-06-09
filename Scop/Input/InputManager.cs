287565f310868598c7c846266dc37c5fe9cf8342
flmarsou <marsouinflorent12@gmail.com>
2026-06-09 12:35:34 +0200

    Add new methods to handle pressed/down

--- Scop/Input/InputManager.cs
+++ Scop/Input/InputManager.cs
@@ -4,8 +4,11 @@ namespace Scop.Input;
 
 public static class InputManager
 {
-    private static readonly bool[] _keys  = new bool[349];
-    private static readonly bool[] _mouse = new bool[13];
+    private static readonly bool[] _keys      = new bool[349];
+    private static readonly bool[] _prevKeys  = new bool[349];
+
+    private static readonly bool[] _mouse     = new bool[13];
+    private static readonly bool[] _prevMouse = new bool[13];
 
     public static void Init(IInputContext input)
     {
@@ -22,6 +25,15 @@ public static class InputManager
         }
     }
 
-    public static bool IsKeyDown(Key key)              => _keys[(int)key];
-    public static bool IsMouseDown(MouseButton button) => _mouse[(int)button];
+    public static void Update()
+    {
+        Array.Copy(_keys,  _prevKeys,  349);
+        Array.Copy(_mouse, _prevMouse, 13);
+    }
+
+    public static bool IsKeyDown(Key key)                 => _keys[(int)key];
+    public static bool IsKeyPressed(Key key)              => _keys[(int)key] && !_prevKeys[(int)key];
+
+    public static bool IsMouseDown(MouseButton button)    => _mouse[(int)button];
+    public static bool IsMousePressed(MouseButton button) => _mouse[(int)button] && !_prevMouse[(int)button];
 }
