55ea7d10e5c26cec74e7d627336e82241b5de540
flmarsou <marsouinflorent12@gmail.com>
2026-06-09 12:38:39 +0200

    Add object getters with index

--- Scop/Object/ObjManager.cs
+++ Scop/Object/ObjManager.cs
@@ -5,9 +5,41 @@ public static class ObjManager
     private static readonly string ContextPath = Path.Combine(AppContext.BaseDirectory, "Assets", "Objects");
 
     private static List<string> _objectFiles = [];
+    private static int          _objectIndex = 0;
 
-    public static void Init()
+    public static void Refresh()
     {
         _objectFiles = ObjUtils.FetchObjectFiles(ContextPath);
+        _objectIndex = 0;
+    }
+
+    public static string? GetCurrent()
+    {
+        if (_objectFiles.Count == 0)
+            return (null);
+
+        return (_objectFiles[_objectIndex]);
+    }
+
+    public static string? GetNext()
+    {
+        if (_objectFiles.Count == 0)
+            return (null);
+
+        if (++_objectIndex >= _objectFiles.Count)
+            _objectIndex = 0;
+
+        return (_objectFiles[_objectIndex]);
+    }
+
+    public static string? GetPrev()
+    {
+        if (_objectFiles.Count == 0)
+            return (null);
+
+        if (--_objectIndex < 0)
+            _objectIndex = _objectFiles.Count - 1;
+
+        return (_objectFiles[_objectIndex]);
     }
 }
