b4145fcb48694cc65068a41e6b99fdbc7b66cd6d
flmarsou <marsouinflorent12@gmail.com>
2026-06-09 12:38:58 +0200

    Add object selection with keys

--- Scop/Core/OnUpdate.cs
+++ Scop/Core/OnUpdate.cs
@@ -1,13 +1,39 @@
 using Scop.Input;
+using Scop.Object;
+using Scop.Utils;
 using Silk.NET.Input;
 
 namespace Scop;
 
 public partial class Program
 {
+    private static string? _selectedObject;
+
     private static void OnUpdate(double deltaTime)
     {
         if (InputManager.IsKeyDown(Key.Escape))
             _window!.Close();
+
+        if (InputManager.IsKeyPressed(Key.R))
+        {
+            try
+            {
+                ObjManager.Refresh();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.ToString());
+
+                _window!.Close();
+            }
+
+            _selectedObject = ObjManager.GetCurrent();
+        }
+        else if (InputManager.IsKeyPressed(Key.Left))
+            _selectedObject = ObjManager.GetPrev();
+        else if (InputManager.IsKeyPressed(Key.Right))
+            _selectedObject = ObjManager.GetNext();
+
+        InputManager.Update();
     }
 }
