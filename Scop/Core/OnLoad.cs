2a8b8881c31fc37f4596ed9cb90ca4878df758b3
flmarsou <marsouinflorent12@gmail.com>
2026-06-09 14:28:58 +0200

    Init OpenGL context and add variables

--- Scop/Core/OnLoad.cs
+++ Scop/Core/OnLoad.cs
@@ -1,26 +1,32 @@
+using System.Drawing;
 using Scop.Input;
 using Scop.Object;
 using Scop.Utils;
 using Silk.NET.Input;
+using Silk.NET.OpenGL;
 
 namespace Scop;
 
 public partial class Program
 {
+    private static GL?  _gl;
+    private static uint _vao;
+    private static uint _vbo;
+    private static uint _program;
+    private static int  _vertexCount;
+
+    private static ObjModel? _selectedModel;
+
     private static void OnLoad()
     {
+        // --- Input ---
         InputManager.Init(_window!.CreateInput());
 
-        try
-        {
-            ObjManager.Refresh();
-        }
-        catch (Exception ex)
-        {
-            Logger.Error(ex.ToString());
+        // --- OpenGL ---
+        _gl = _window.CreateOpenGL();
 
-            _window!.Close();
-        }
+        // --- Object ---
+        ObjManager.Refresh();
 
         _selectedModel = ObjParser.Parse(ObjManager.GetCurrent());
     }
